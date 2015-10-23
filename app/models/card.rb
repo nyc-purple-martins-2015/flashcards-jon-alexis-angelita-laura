ada1e48a4f7f2a5f543e7fbdb50ebef7f27323f4
Alexis La Porte & Jon Star <pair+aklap.jonathanstar@devbootcamp.com>
2015-10-23 19:09:57 -0400

    add end round button

--- app/models/card.rb
+++ app/models/card.rb
@@ -8,4 +8,19 @@ class Card < ActiveRecord::Base
     [self.answer, self.false_answer_1, self.false_answer_2, self.false_answer_3].shuffle
   end
 
+def update_guess
+  correct = []
+    these_cards = deck
+    while these_cards.length > 0
+      these_cards.each do |c|
+        if @choice == c.answer
+          guess.solved = true
+          guess.attempts += 1
+          correct << c
+          these_cards.delete(c)
+        else
+          guess.attempts += 1
+        end
+      end
+    end
 end
