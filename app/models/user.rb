77b157d4c8baca965c40e4f1d4ef76f6b14a6b7e
Jon Star <email": null>
2015-10-23 16:26:44 -0400

    add bcryt stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ActiveRecord::Base
   has_many :rounds
   has_many :guesses, through: :rounds
   has_many :decks, through: :rounds
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
