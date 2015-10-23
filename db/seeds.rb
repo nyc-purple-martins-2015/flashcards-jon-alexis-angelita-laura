3d0cb3d6e7eec9ecab6364fe6fd1294b39cd1e6b
Jon Star <email": null>
2015-10-23 16:30:51 -0400

    delete silly thing in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,3 @@ j_round = Round.create({:user_id=> 1, deck_id: 1})
 deck = Deck.create({:category=>'cats'})
 c_card_1 = Card.create({:question => 'how many cats are the best?', :answer => 'all of them', :deck_id => 1})
 our_guess = Guess.create({:card_id=>1,:round_id=>1, :attempts=>1, :solved=> false})
-
-jon.password="password"
