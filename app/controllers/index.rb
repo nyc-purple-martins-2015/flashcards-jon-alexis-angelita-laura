e83b69bac5c173e2f460af73b3afd1ada1baacbb
Angelita Graves <angelita.l.graves@vanderbilt.edu>
2015-10-23 18:18:41 -0400

    begin round logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,10 @@
 get '/' do
-  "hello world"
+  @decks = Deck.all
+  erb :'decks/index'
 end
+
+get '/decks/:id' do
+ @deck = Deck.find(params[:id])
+ erb :'decks/show'
+end
+
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,4 +3,23 @@ class Deck < ActiveRecord::Base
   has_many :cards
   has_many :guesses, through: :cards
   has_many :users, through: :rounds
+
+  def shuffle
+    @deck.shuffle
+  end
+
+  def remove_card(card)
+    if correct_guess == true
+      card.destroy
+    end
+  end
+
+  def is_empty?
+    @deck.count == 0
+  end
+end
+
+
+
+
 end
