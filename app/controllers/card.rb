f081ea0a04f79de4dd393fa6765fdd5506b17b34
jonstar <jsskis7@gmail.com>
2015-10-24 17:37:34 -0400

    add requested instance variable choice

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -5,6 +5,7 @@ end
 
 post '/cards/:id' do
   @card = Card.find(params[:id])
+  @choice = params[:choice]
   unless  params[:choice].eql?(params[:answer])
     @incorrect = true
     return erb :'card/card_question'
