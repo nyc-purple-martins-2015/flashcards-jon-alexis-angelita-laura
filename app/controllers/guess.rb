d9788c3e9b03a78610ce9bace9828ca50049f3c8
LAURA EVANS <LKSEVANS12@GMAIL.com>
2015-10-25 14:37:26 -0400

    add guess logic

--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -1,11 +1,14 @@
 post '/guesses' do
   input = params[:choice]
   @card = Card.find_by(id: params[:card_id])
+  @round = Round.find(session[:round_id])
   @correct = @card.correct?(input)
   @guess = Guess.create(card_id:@card.id, round_id: session[:round_id], solved: @correct)
 
-  unless @correct
-    return erb :'card/card_question'
-  end
+  if @correct
     erb :'card/card_answer'
+  else
+    erb :'card/card_question'
+    redirect "/round/#{@round.id}/cards/#{@card.id}"
+  end
 end
